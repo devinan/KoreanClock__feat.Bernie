2b484449d1736ab937d8d88f7e6bbc569d4e6fce
rlawlsgus <jh0623.kim@gmail.com>
2020-04-26 00:41:39 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 Creating Typescript Korean Clock Recommended By Bernie
 
 코드가 너무 더러워서 다시 만들고 싶네요 ㅠㅠ 좋은 아이디어 있으시면 알려주세요!!
-
-수정해야 할 내용:
-  1. 오후 12시에는 "시" 불이 안들어옴
-  2. 오후 12시에는 시간 표시가 안됨
--- index.ts
+++ index.ts
@@ -3,74 +3,144 @@ const widget = document.getElementsByClassName("widget");
 let htmlElements = [];
 const LIGHT = "on";
 
-const amClock = (time: object) => {};
-const pmClock = (time: object) => {
-  const minus12 = time["hour"] - 12;
+const setHour = (time: object): void => {
+  let timeset12: number;
   let static: string;
-  if (minus12 < 10) {
-    static = `0${minus12}`;
+
+  if (time["ampm"] === "pm") {
+    timeset12 = time["hour"] - 12;
+  } else if (time["ampm"] === "am") {
+    timeset12 = time["hour"];
+  }
+
+  if (timeset12 < 10) {
+    static = `0${timeset12}`;
   } else {
-    static = String(minus12);
+    static = String(timeset12);
   }
   const hourCase = static.split("");
 
-  htmlElements[5].classList = LIGHT;
   if (hourCase[0] === "1" || static === "00") {
-    htmlElements[14].classList = LIGHT;
+    htmlElements[14].classList.add(LIGHT);
   }
   switch (hourCase[1]) {
     case "0":
       if (hourCase[0] === "0") {
-        htmlElements[26].classList = LIGHT;
+        htmlElements[16].classList.add(LIGHT);
       }
       break;
     case "1":
       if (hourCase[0] === "0") {
-        htmlElements[0].classList = LIGHT;
+        htmlElements[0].classList.add(LIGHT);
       } else {
-        htmlElements[20].classList = LIGHT;
+        htmlElements[15].classList.add(LIGHT);
       }
       break;
     case "2":
-      htmlElements[6].classList = LIGHT;
+      htmlElements[1].classList.add(LIGHT);
       break;
     case "3":
-      htmlElements[12].classList = LIGHT;
+      htmlElements[2].classList.add(LIGHT);
       break;
     case "4":
-      htmlElements[18].classList = LIGHT;
+      htmlElements[3].classList.add(LIGHT);
       break;
     case "5":
-      htmlElements[24].classList = LIGHT;
-      htmlElements[30].classList = LIGHT;
+      htmlElements[4].classList.add(LIGHT);
+      htmlElements[5].classList.add(LIGHT);
       break;
     case "6":
-      htmlElements[1].classList = LIGHT;
-      htmlElements[7].classList = LIGHT;
+      htmlElements[6].classList.add(LIGHT);
+      htmlElements[7].classList.add(LIGHT);
       break;
     case "7":
-      htmlElements[13].classList = LIGHT;
-      htmlElements[19].classList = LIGHT;
+      htmlElements[8].classList.add(LIGHT);
+      htmlElements[9].classList.add(LIGHT);
       break;
     case "8":
-      htmlElements[25].classList = LIGHT;
-      htmlElements[31].classList = LIGHT;
+      htmlElements[10].classList.add(LIGHT);
+      htmlElements[11].classList.add(LIGHT);
       break;
     case "9":
-      htmlElements[2].classList = LIGHT;
-      htmlElements[8].classList = LIGHT;
+      htmlElements[12].classList.add(LIGHT);
+      htmlElements[13].classList.add(LIGHT);
       break;
   }
-  console.log(hourCase);
 };
 
-const getTime = (): object => {
+const setMinute = (time: object): void => {
+  const minute = time["min"];
+  let static: string;
+  if (minute < 10) {
+    static = `0${minute}`;
+  } else {
+    static = String(minute);
+  }
+
+  const minuteCase = static.split("");
+  htmlElements[23].classList.add(LIGHT);
+  switch (minuteCase[0]) {
+    case "1":
+      break;
+    case "2":
+      htmlElements[19].classList.add(LIGHT);
+      break;
+    case "3":
+      htmlElements[20].classList.add(LIGHT);
+      break;
+    case "4":
+      htmlElements[21].classList.add(LIGHT);
+      break;
+    case "5":
+      htmlElements[22].classList.add(LIGHT);
+      break;
+    default:
+      htmlElements[23].classList.remove(LIGHT);
+      break;
+  }
+
+  switch (minuteCase[1]) {
+    case "1":
+      htmlElements[25].classList.add(LIGHT);
+      break;
+    case "2":
+      htmlElements[26].classList.add(LIGHT);
+      break;
+    case "3":
+      htmlElements[27].classList.add(LIGHT);
+      break;
+    case "4":
+      htmlElements[28].classList.add(LIGHT);
+      break;
+    case "5":
+      htmlElements[29].classList.add(LIGHT);
+      break;
+    case "6":
+      htmlElements[31].classList.add(LIGHT);
+      break;
+    case "7":
+      htmlElements[32].classList.add(LIGHT);
+      break;
+    case "8":
+      htmlElements[33].classList.add(LIGHT);
+      break;
+    case "9":
+      htmlElements[34].classList.add(LIGHT);
+      break;
+    default:
+      htmlElements[35].classList.remove(LIGHT);
+      break;
+  }
+};
+
+const getTime = (): void => {
+  htmlElements = [];
   const currentTime = new Date();
   const hour = currentTime.getHours();
   const min = currentTime.getMinutes();
   const ampm = hour >= 12 ? "pm" : "am";
   const time = { hour, min, ampm };
-  return time;
+  getElements(time);
 };
 
 const getElements = (time: object): void => {
@@ -79,21 +149,26 @@ const getElements = (time: object): void => {
       htmlElements.push(widget[0].children[i]);
     }
   }
-  htmlElements[3].classList = LIGHT;
-  htmlElements[32].classList = LIGHT;
-  htmlElements[35].classList = LIGHT;
+  htmlElements.sort((a, b) => {
+    return a.classList[0] - b.classList[0];
+  });
+
+  htmlElements[17].classList.add(LIGHT);
+  htmlElements[18].classList.add(LIGHT);
+  htmlElements[35].classList.add(LIGHT);
+
   if (time["ampm"] === "am") {
-    amClock(time);
+    htmlElements[24].classList.add(LIGHT);
   } else {
-    pmClock(time);
+    htmlElements[30].classList.add(LIGHT);
   }
-  console.log(htmlElements);
+  setHour(time);
+  setMinute(time);
 };
 
 const init = () => {
-  //const currentTime = setInterval(getTime, 1000);
-  const currentTime = getTime();
-  getElements(currentTime);
+  setInterval(getTime, 1000);
+  //getTime();
 };
 
 init();
